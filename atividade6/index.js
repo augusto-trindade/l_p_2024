03c10cefeb5168fcda782354c507af681d828267
Augusto Trindade <augusto.trindades@gmail.com>
2024-04-18 00:19:04 -0300

    Code modifies

--- atividade6/index.js
+++ atividade6/index.js
@@ -5,25 +5,23 @@ class Estudante{
         this.email = email
         this.ra = ra
         this.curso = curso
-        this.disciplinas = disciplinas
+        this.disciplinas = []
     }
-    primeiraDisciplina(disciplinas){
-        disciplinas.forEach((valor, indice) => {
-            if(indice == 0){
-                console.log(`A primeira disciplina é ${this.disciplinas[0]}`)
+    primeiraDisciplina(){
+            console.log(`A primeira disciplina é ${this.disciplinas[0]}`)
             }
-        })
-    }
-    ultimaDisciplina(disciplinas){
-        disciplinas.forEach((valor, indice) => {
-            if(indice == lista.length-1){
-                console.log(`A última disciplina é ${this.disciplinas[lista.length-1]}`)
-            }
-        })
+    ultimaDisciplina(){
+            console.log(`A última disciplina é ${this.disciplinas[this.disciplinas.length-1]}`)
     }
 }
 
-const pessoa1 = new Estudante("pessoa1", "pessoa1@gmail.com", 202401, "ADS", ["LP", "Calculo", "SI", "SO", "ES"])
-const pessoa2 = new Estudante("pessoa2", "pessoa2@outlook.com", 202402, "ADM", ["MKT", "RH", "Financeiro", "Operações"])
+const pessoa1 = new Estudante("pessoa1", "pessoa1@gmail.com", 202401, "ADS")
+pessoa1.disciplinas = ["LP", "Calculo", "SI", "SO", "ES"]
+const pessoa2 = new Estudante("pessoa2", "pessoa2@outlook.com", 202402, "ADM")
+pessoa2.disciplinas =  ["MKT", "RH", "Financeiro", "Operações"]
+
+console.log(pessoa1)
+console.log(pessoa2)
 
 pessoa1.primeiraDisciplina()
+pessoa2.ultimaDisciplina()
