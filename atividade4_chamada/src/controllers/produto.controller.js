32d987a3aefb96014e21a7f04727360804772973
Augusto Trindade <augusto.trindades@gmail.com>
2024-06-03 22:40:27 -0300

    fazendo a parte dois do código

--- atividade4_chamada/src/controllers/produto.controller.js
+++ atividade4_chamada/src/controllers/produto.controller.js
@@ -3,7 +3,11 @@ import Produto from '../models/produto.model.js'
 
 export default class ProdutoController{
     static async index(req,res) {
-        const produtos = await Produto.findMany()
+        const produtos = await Produto.findMany({
+            include: {
+                fotos: true
+            }
+        })
         res.json(produtos)
     }
 
--- atividade4_chamada/src/routes/index.js
+++ atividade4_chamada/src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import produtoRouter from './produto.route.js'
+import fotoRouter from './produto.foto.js'
 
 const router = Router()
 
 router.use('/produtos', produtoRouter)
+router.use('/fotos', fotoRouter)
 
 export default router
